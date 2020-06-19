fbecf06cbcc15adeadfca71b1142dbd3270e2b8b
freglyc <cdf7@rice.edu>
2020-06-19 16:47:55 -0500

    clean up server

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"flag"
 	"github.com/freglyc/tsuro/server"
+	"log"
 	"net/http"
 )
 
+var addr = flag.String("addr", ":8080", "http service address")
+
+func serveHome(response http.ResponseWriter, request *http.Request) {
+	if request.URL.Path != "/" {
+		http.Error(response, "Not found", http.StatusNotFound)
+		return
+	}
+	if request.Method != "GET" {
+		http.Error(response, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	_, _ = response.Write([]byte("Tsuro Server"))
+}
+
 func main() {
+	flag.Parse()
 	hub := server.NewHub()
 	go hub.Run()
+	http.HandleFunc("/", serveHome)
 	http.HandleFunc("/ws", func(response http.ResponseWriter, request *http.Request) {
 		server.ServeWs(hub, response, request)
 	})
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
--- backend/server/handler.go
+++ backend/server/handler.go
@@ -8,17 +8,14 @@ import (
 type GameHandler struct {
 	Clients map[*Client]bool // Holds mapping of clients subscribed to the game
 	Game    *tsuro.Game      // Game
-
-	Timer *time.Timer // Turn timer that may or may not be enabled
-	End   time.Time   // Timer end time
+	Timer   *time.Timer      // Turn timer that may or may not be enabled
+	End     time.Time        // Timer end time
 }
 
 func NewHandler(game *tsuro.Game) *GameHandler {
 	return &GameHandler{
 		Clients: make(map[*Client]bool),
 		Game:    game,
-
-		Timer: nil,
-		End:   nil,
+		Timer:   nil,
 	}
 }
