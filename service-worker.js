2c70d481b2c11798b3eb037bd8d5e00142822d27
freglyc <31714410+freglyc@users.noreply.github.com>
2020-07-08 15:24:36 +0000

    Deploying to gh-pages from  @ e878455d8693d25a2b28bd04175794fc57d62a48 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a7049cf2990380c010359bddfd71ffb.js"
+  "/precache-manifest.e1fe28aecbc805f47b22edb4bf2b451f.js"
 );
 
 self.addEventListener('message', (event) => {
